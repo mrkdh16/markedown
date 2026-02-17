2fc2029bdd54f2bdf04ee30bb5c234c462580433
Mark Rhee <mark.dh.rhee@gmail.com>
2026-02-16 18:31:07 -0800

    Quartz sync: Feb 16, 2026, 6:31 PM

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -20,7 +20,7 @@ export default ((opts?: Options) => {
         <ul>
           {Object.entries(links).map(([text, link]) => (
             <li>
-              <a href={link} target="_self">{text}</a>
+              <a href={link} target="_blank" rel="noopener noreferrer">{text}</a>
             </li>
           ))}
         </ul>

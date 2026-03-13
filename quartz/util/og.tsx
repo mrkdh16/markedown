720976d0f7317f6e4cf8330f918dbc0954842e33
Mark Rhee <mark.dh.rhee@gmail.com>
2026-03-12 19:53:06 -0700

    Quartz sync: Mar 12, 2026, 7:53 PM

--- quartz/util/og.tsx
+++ quartz/util/og.tsx
@@ -100,8 +100,8 @@ export async function fetchTtf(
   )
   const css = await cssResponse.text()
 
-  // Extract .ttf url from css file
-  const urlRegex = /url\((https:\/\/fonts.gstatic.com\/s\/.*?.ttf)\)/g
+  // Extract font url from css file (ttf or woff2 depending on user-agent)
+  const urlRegex = /url\((https:\/\/fonts.gstatic.com\/s\/.*?\.(ttf|woff2))\)/g
   const match = urlRegex.exec(css)
 
   if (!match) {
